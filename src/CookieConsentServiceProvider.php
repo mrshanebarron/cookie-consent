743f9c272b0450ee18423f5ce802fcfd998c0944
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 18:58:09 -0500

    Fix PHP syntax - use PascalCase for all class names

--- src/CookieConsentServiceProvider.php
+++ src/CookieConsentServiceProvider.php
@@ -3,14 +3,36 @@
 namespace MrShaneBarron\CookieConsent;
 
 use Illuminate\Support\ServiceProvider;
+use MrShaneBarron\CookieConsent\Livewire\CookieConsent;
+use MrShaneBarron\CookieConsent\View\Components\CookieConsent as BladeCookieConsent;
 
 class CookieConsentServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-cookie-consent.php', 'sb-cookie-consent');
+    }
+
     public function boot(): void
     {
-        if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::component('sb-cookie-consent', Livewire\CookieConsent::class);
-        }
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-cookie-consent');
+
+        if (class_exists(\Livewire\Livewire::class)) {
+            \Livewire\Livewire::component('sb-cookie-consent', CookieConsent::class);
+        }
+
+        $this->loadViewComponentsAs('ld', [
+            BladeCookieConsent::class,
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sb-cookie-consent.php' => config_path('sb-cookie-consent.php'),
+            ], 'sb-cookie-consent-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sb-cookie-consent'),
+            ], 'sb-cookie-consent-views');
+        }
     }
 }
